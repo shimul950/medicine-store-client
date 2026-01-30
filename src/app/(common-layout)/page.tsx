09ed861408ef9b9f864b222f52a1b63ab1b85a8e
Sayedul Islam <shimulmazumder950@gmail.com>
2026-01-30 12:52:18 +0600

    simple google login setup

--- src/app/(common-layout)/page.tsx
+++ src/app/(common-layout)/page.tsx
@@ -1,6 +1,22 @@
 import HeroCarousel from "@/components/modules/home/slidder";
+import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
-export default function Home() {
+
+export default async function Home() {
+ 
+  const cookieStore = await cookies();
+
+  const res = await fetch("http://localhost:5000/api/auth/get-session",{
+    headers:{
+      Cookie : cookieStore.toString()
+    }
+  })
+
+  const session = await res.json()
+
+  console.log(session);
+
   return (
     <div> 
       <HeroCarousel/>
